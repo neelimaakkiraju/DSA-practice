eecf9fff242a39ffd7fb4f80a44f33ae45608627
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-10-07 15:48:03 +0530

    solved min, max in array

---
+++ problem4.js
@@ -0,0 +1,9 @@
+//finding max and min element in array
+
+const myArr = [-1,0,1,5,6,8,9,10,-15]
+
+const result = Math.max(...myArr)
+const result2 = Math.min(...myArr)
+console.log(result)
+console.log(result2)
+
