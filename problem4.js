477f536d2de5e580836527b0beef9b3a7deded1e
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-10-09 11:47:17 +0530

    palindrome with number

--- problem4.js
+++ problem4.js
@@ -7,3 +7,13 @@ const result2 = Math.min(...myArr)
 console.log(result)
 console.log(result2)
 
+//palindrome number
+
+function isPalindrome(num){
+     let result = num<= 0 ? false : +num.split("").reverse().join("")
+     let result2 = +num.split("") // adding "+" to convert the the string number to number
+     console.log(typeof result2)
+     console.log(result)
+}
+
+isPalindrome("0")
