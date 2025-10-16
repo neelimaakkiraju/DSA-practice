669cdc8e3b94675f4fba8a21a2e8d62357727876
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-10-16 15:54:14 +0530

    solved anagram check problem using optimized hash map pattern.

--- 6problem.js
+++ 6problem.js
@@ -1,12 +1,33 @@
 //valid anagram 
 
-function isAnagram(a, b){
-      let ans1 = a.split("").sort().join()
-      let ans2 = b.split("").sort().join()
-     return ans1 === ans2
+// function isAnagram(a, b){
+//       let ans1 = a.split("").sort().join()
+//       let ans2 = b.split("").sort().join()
+//      return ans1 === ans2
    
+// }
+
+// console.log(isAnagram("anagram","nagaram"))
+
+
+//using hash map DSA 
+//space complexity O(n)
+//time complexityO(n log n)
+
+function checkAnagram(c,d){
+      if (c.length!== d.length) return false;
+      let obj1= {}
+      let obj2 = {}
+
+      for(let i=0; i<c.length;i++){
+            obj1[c[i]] = (obj1[c[i]]||0) + 1;
+            obj2[c[i]] = (obj2[c[i]]|| 0) + 1;
+      }
+
+      for(const key in obj1){
+          if(obj1[key] !== obj2[key]) return false
+      }
+      return true
 }
 
-console.log(isAnagram("anagram","nagaram"))
-
-
+console.log(checkAnagram("anagram","nagarm"))
