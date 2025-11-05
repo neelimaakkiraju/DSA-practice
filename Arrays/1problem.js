7e6c805007b1e6d5e5f90717307eb62e5c70602e
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-11-05 12:12:47 +0530

    solved reverse array using array methods

--- Arrays/1problem.js
+++ Arrays/1problem.js
@@ -1,9 +1,27 @@
 //Reverse Array
-
+//brute force method
 
 function reverseArr(arr){
-    let result = arr.reverse()
+    let result = arr.reverse().join(" ")
     return result
 }
 
-console.log(reverseArr([2,3,5,6,8,9,1]))
+console.log(reverseArr([1, 4, 3, 2, 6, 5]))
+
+
+//expected O(n)
+function reverseArray(arr) {
+
+    let left = 0, right = arr.length - 1;
+    while (left < right) {
+        
+        [arr[left], arr[right]] = [arr[right], arr[left]];
+        
+        left++;
+        right--;
+    }
+}
+
+const arr = [1, 4, 3, 2, 6, 5];
+reverseArray(arr);
+console.log(arr.join(" "));
