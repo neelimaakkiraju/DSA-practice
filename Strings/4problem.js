b0d28f9e90eab161942b1a1af223e040d15b94fc
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-11-10 15:27:40 +0530

    solved search insert position

---
+++ Strings/4problem.js
@@ -0,0 +1,28 @@
+// var searchInsert = function(nums, target) {
+
+//     let result = nums.indexOf(target)
+
+//     return result
+    
+// };
+
+
+//search insert position
+
+function searchInsert(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) return mid;
+    else if (nums[mid] < target) left = mid + 1;
+    else right = mid - 1;
+  }
+
+  
+  return left;
+}
+
+console.log(searchInsert([1,3,5,6],7))
