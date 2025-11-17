49a5cb1bc22b88e6ba5dd683d905c8ac35a35cdf
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-11-17 16:49:57 +0530

    solved fibonacci series using while loop

---
+++ Search & Sort/12problem.js
@@ -0,0 +1,29 @@
+// fibonacci generator
+
+
+// function generateFibonacci(num){
+
+//     let result = []
+//     for(let i = 0; i<num.length;i++){
+//           for(let j = i+1; j<num.length;j++){
+//             let final = num[i]+ num[j]
+//             result.push(final)
+//           }
+//     }
+//     return result
+// }
+
+// console.log(generateFibonacci(5))
+
+
+
+var fibGenerator = function*() {
+     let a = 0, b = 1;
+
+    while (true) {
+        yield a;        // yield current Fibonacci number
+        [a, b] = [b, a + b];  // update to next
+    }
+};
+
+console.log(fibGenerator())
