c5a0930bba074801e79207b485346c114076c9da
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-11-26 12:22:57 +0530

    solved second largest number problem using loops and optimized solution

--- NamsteDSA/4problem.js
+++ NamsteDSA/4problem.js
@@ -1,15 +1,22 @@
 // find second largest number in an array
 
 function findSecondLargest(arr){
-    let secondLargest = []
-    let count = -Infinity
+
+    if(arr.length<2) return null
+
+    let secondLargest = -Infinity
+    let firstLargest = -Infinity
     for(let i =0; i<arr.length;i++){
-        if(arr[i]>count){
-            count= arr[i]
+        if(arr[i]>firstLargest){
+            secondLargest = firstLargest;
+            firstLargest = arr[i]
             
+        } else if(arr[i]>secondLargest && arr[i] !== firstLargest){
+            secondLargest = arr[i]
         }
     }
-    return count
+    return secondLargest
 }
 
-console.log(findSecondLargest([5,6,7,3,9,2]))
+console.log(findSecondLargest([5,6,7,3,9,2,9]))
+console.log(findSecondLargest([2]))
