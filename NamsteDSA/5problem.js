e5b18c7f91b82c388ae63dabf2f1e9dd08790ef1
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-11-28 16:57:50 +0530

    nested loops with different values

--- NamsteDSA/5problem.js
+++ NamsteDSA/5problem.js
@@ -4,9 +4,23 @@
 function nestedLoop(num){
     for(let i = 0; i<num; i++){
         for(let j = 0; j< num;j++){
-            console.log(i,j)
+            console.log("i="+i,"j="+j)
         }
     }
 }
 
 console.log(nestedLoop(3))
+
+
+//loops practice
+
+
+function nestedLoop2(num){
+    for(let i = 0; i<num; i++){
+        for(let j = 0; j < i;j++){
+            console.log("i="+i,"j="+j)
+        }
+    }
+}
+
+console.log(nestedLoop2(3))
