d6b49cab40b8f4af6d80b1ba471b19893b7626cf
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-11-29 12:22:19 +0530

    practiced different nested loops

--- NamsteDSA/5problem.js
+++ NamsteDSA/5problem.js
@@ -17,10 +17,34 @@ console.log(nestedLoop(3))
 
 function nestedLoop2(num){
     for(let i = 0; i<num; i++){
-        for(let j = 0; j < i;j++){
+        for(let j = 0; j <= i;j++){
             console.log("i="+i,"j="+j)
         }
     }
 }
 
 console.log(nestedLoop2(3))
+
+//j value as i
+
+function nestedLoop3(num){
+    for(let i = 0; i<num; i++){
+        for(let j = i; j >= 0;j--){
+            console.log("i="+i,"j="+j)
+        }
+    }
+}
+
+console.log(nestedLoop3(6))
+
+//i value as j
+
+function nestedLoop4(num){
+    for(let i = 5; i>0; i--){
+        for(let j = 0; j <i;j++){
+            console.log("i="+i,"j="+j)
+        }
+    }
+}
+
+console.log(nestedLoop4(4))
