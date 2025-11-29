6a88747f86fb192c81bd71cb9d443a203e37360e
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-11-29 16:37:56 +0530

    practiced patterns with nested loops

--- NamsteDSA/6problem.js
+++ NamsteDSA/6problem.js
@@ -2,8 +2,60 @@
 
 for(let i = 0;i<5;i++){
     let star = ""
-    for(let j = 0; j<i;j++){
+    for(let j = 0; j<5;j++){
        star = star+ "* "
     }
     console.log(star)
 }
+
+
+for(let i = 0;i<5;i++){
+    let star = ""
+    for(let j = 0; j<i+1;j++){
+       star = star+ "* "
+    }
+    console.log(star)
+}
+
+
+
+for(let i = 0;i<5;i++){
+    let result = ""
+    for(let j = 1; j<i+1;j++){
+      result = result+ j +" "
+    }
+    console.log(result)
+}
+
+
+for(let i = 0;i<5;i++){
+    let result = ""
+    for(let j = 0; j<i;j++){
+      result = result+ i +" "
+    }
+    console.log(result)
+}
+
+
+for(let i = 0;i<5;i++){
+    let result = ""
+    for(let j = 0; j<5-i;j++){
+      result = result+ j +" "
+    }
+    console.log(result)
+}
+
+
+
+
+for(let i = 0;i<5;i++){
+    let result = ""
+    for(let j = 0; j<5-i;j++){
+      result = result+"* "
+    }
+    console.log(result)
+}
+
+
+
+
