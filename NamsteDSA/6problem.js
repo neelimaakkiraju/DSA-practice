75308b9a59994471dde4b3c7a474633925d7eacb
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-11-29 17:53:34 +0530

    practiced  star and number patterns using loops

--- NamsteDSA/6problem.js
+++ NamsteDSA/6problem.js
@@ -50,7 +50,10 @@ for(let i = 0;i<5;i++){
 
 for(let i = 0;i<5;i++){
     let result = ""
-    for(let j = 0; j<5-i;j++){
+    for(let j = 0; j<5-(i+1);j++){
+      result = result+ ". "
+    }
+    for(let k = 0; k<i+1;k++){
       result = result+"* "
     }
     console.log(result)
@@ -59,3 +62,37 @@ for(let i = 0;i<5;i++){
 
 
 
+let n = 5;
+
+for(let i = 0; i<n;i++){
+
+    let row = ""
+    let toggle = 1
+    for(let j = 0; j<i+1; j++){
+        row = row + toggle + " "
+       if(toggle == 1){
+           toggle = 0
+       }else{
+        toggle =1
+       }
+    }
+    console.log(row)
+}
+
+
+let x = 5;
+let toggle = 1
+for(let i = 0; i<x;i++){
+
+    let row = ""
+    
+    for(let j = 0; j<i+1; j++){
+        row = row + toggle + " "
+       if(toggle == 1){
+           toggle = 0
+       }else{
+        toggle =1
+       }
+    }
+    console.log(row)
+}
