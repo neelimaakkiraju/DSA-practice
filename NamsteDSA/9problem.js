6004f9bc8487ee7c341c2c71ad827a3115c8b6c1
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-12-02 18:27:55 +0530

    solved reverse a number problem using while loop

--- NamsteDSA/9problem.js
+++ NamsteDSA/9problem.js
@@ -1,3 +1,20 @@
 // reverse a number
 
 
+var numberReverse = function(x) {
+    if (x < 0) {
+        return false;
+    }
+
+    let reverse = 0;
+    let xcopy = x;
+
+    while (x > 0) {
+        reverse = (reverse * 10) ;
+        x = Math.floor(x / 10);
+    }
+
+    return reverse === xcopy;    
+};
+
+console.log(numberReverse(2375))
