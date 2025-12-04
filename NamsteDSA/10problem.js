428d3881cb2f5c8ccb15b303c3dd5dd8b867060a
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-12-04 12:34:27 +0530

    solved remove duplicates in sorted array

---
+++ NamsteDSA/10problem.js
@@ -0,0 +1,30 @@
+// remove duplicates in an array
+
+function removeDuplicates(arr){
+     let x = 0;
+    for(let i=0; i<arr.length;i++){
+       
+        if(arr[i]> arr[x]){
+            x = x+1
+            arr[x] = arr[i]
+        }
+    }
+    return x+1
+}
+
+console.log(removeDuplicates([1,2,3,3,5,5,7,7,9,9]))
+
+
+function removeDuplicates(arr) {
+  let x = 0;
+    for(let i=0; i<arr.length;i++){
+       
+        if(arr[i]> arr[x]){
+            x = x+1
+            arr[x] = arr[i]
+        }
+    }
+    return x+1
+}
+
+console.log(removeDuplicates([1,1,2,2,4,4,5,5,6,6]))
