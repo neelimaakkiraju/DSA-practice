5bf2d6f61f8e98fd294ea38f68e7981116c75f65
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-12-09 12:20:23 +0530

    solved find taget element using linear search

---
+++ NamsteDSA/SearchAndSort/linearSearch.js
@@ -0,0 +1,12 @@
+// find target element in an array
+
+
+function findElement(arr , target){
+    for(let i =0 ; i<arr.length; i++){
+         if(arr[i] === target){
+            return i
+         }
+    }return -1
+}
+
+console.log(findElement([2,4,6,7,2], 8))
