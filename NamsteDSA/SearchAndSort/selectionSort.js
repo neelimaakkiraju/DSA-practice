e51ee3b2d77179df3e84c59f91dc6b4b2477eacb
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-12-12 17:49:18 +0530

    sorted array elements using selection sort

---
+++ NamsteDSA/SearchAndSort/selectionSort.js
@@ -0,0 +1,19 @@
+// sort array elements using selection sort
+
+
+function sortElement(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    let temp = arr[i];
+    arr[i] = arr[min];
+    arr[min] = temp;
+  }
+  return arr;
+}
+
+console.log(sortElement([3, 6, 2, 8, 4, 9]));
