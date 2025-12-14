b5ca8eecb174c01e893714e8b87d4c4de65edc17
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-12-14 18:48:51 +0530

    practiced merge sort

---
+++ NamsteDSA/SearchAndSort/mergeSort.js
@@ -0,0 +1,16 @@
+// merge and sort 2 arrays using merge sort
+
+function mergeSort(arr){
+
+    let mid = arr.length/2;
+
+    let left  = mergeSort(arr.slice(0,mid))
+
+    let right = mergeSort(arr.slice(mid))
+
+    let result = mergeSort(...right, ...left)
+
+    return result
+}
+
+console.log(mergeSort([3,6,8,9,3,4,2,1]))
