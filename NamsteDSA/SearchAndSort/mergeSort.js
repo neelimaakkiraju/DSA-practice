ae1ddbf5ea719b72a19ea40ee352764c66a5709b
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-12-15 12:13:26 +0530

    solved sorting array using merge sort

--- NamsteDSA/SearchAndSort/mergeSort.js
+++ NamsteDSA/SearchAndSort/mergeSort.js
@@ -1,16 +1,35 @@
 // merge and sort 2 arrays using merge sort
 
 function mergeSort(arr){
-
-    let mid = arr.length/2;
+if(arr.length<=1) return arr
+    let mid = Math.floor( arr.length/2);
 
     let left  = mergeSort(arr.slice(0,mid))
 
     let right = mergeSort(arr.slice(mid))
 
-    let result = mergeSort(...right, ...left)
+    return merge(left , right)
 
-    return result;
+}
+
+function merge(left , right){
+    let res = [];
+
+    let i = 0;
+    let j = 0;
+     
+    while(i<left.length && j < right.length){
+        if(left[i] < right[j]){
+            res.push(left[i])
+            i++
+        }
+        else{
+            res.push(right[j])
+            j++
+        }
+    }
+
+    return [...res, ...left.slice(i), ...right.slice(j)]
 }
 
 console.log(mergeSort([3,6,8,9,3,4,2,1]))
