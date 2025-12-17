72553bc57899c8c5f1df153858e0d9f7608b7db3
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-12-17 16:13:14 +0530

    fixed linked list code bugs

--- NamsteDSA/LinkedList/list.js
+++ NamsteDSA/LinkedList/list.js
@@ -1,27 +1,31 @@
-// LinkedList design
 
-
-var MyLinkedList = function(val) {
+function Node(val) {
     this.val = val;
     this.next = null
 };
 
 var MyLinkedList = function(){
     this.head = null;
-    this.size = 0
-
+    this.size = 0;
 }
-
+/** 
+ * @param {number} index
+ * @return {number}
+ */
 MyLinkedList.prototype.get = function(index) {
-    if(index<0 || index>= size) return -1
+    if(index<0 || index >= this.size) return -1;
+
     let curr = this.head;
-    for(let i = 0; i<inde;i++){
+    for(let i = 0; i<index;i++){
         curr= curr.next;
     }
     return curr.val;
 };
 
-
+/** 
+ * @param {number} val
+ * @return {void}
+ */
 MyLinkedList.prototype.addAtHead = function(val) {
     let newNode = new Node(val)
     newNode.next = this.head;
@@ -29,6 +33,10 @@ MyLinkedList.prototype.addAtHead = function(val) {
     this.size++
 };
 
+/** 
+ * @param {number} val
+ * @return {void}
+ */
 MyLinkedList.prototype.addAtTail = function(val) {
     let newNode = new Node(val);
     if(this.head === null){
@@ -40,11 +48,19 @@ MyLinkedList.prototype.addAtTail = function(val) {
         }
         curr.next = newNode
     }
+
+    this.size++
+
 };
 
-
+/** 
+ * @param {number} index 
+ * @param {number} val
+ * @return {void}
+ */
 MyLinkedList.prototype.addAtIndex = function(index, val) {
     let newNode = new Node(val);
+    if(index < 0 || index >= this.size) return
     if(index === 0 ){
         this.addAtHead(val);
         return
@@ -60,21 +76,35 @@ MyLinkedList.prototype.addAtIndex = function(index, val) {
         }
         newNode.next = curr.next;
         curr.next = newNode
+        this.size++
     }
-    this.size++
+    
 };
 
-
+/** 
+ * @param {number} index
+ * @return {void}
+ */
 MyLinkedList.prototype.deleteAtIndex = function(index) {
-    if(index<0 || index<= size) return;
+    if(index<0 || index<= this.size) return;
     if(index === 0){
         this.head = this.head.next;
     }else{
         let curr = this.head;
-        for(let i =0 ; i<index;i++){
+        for(let i =0 ; i<index-1;i++){
             curr = curr.next
         }
-        curr = curr.next.next
+        curr.next = curr.next.next
     }
     this.size--
 };
+
+/** 
+ * Your MyLinkedList object will be instantiated and called as such:
+ * var obj = new MyLinkedList()
+ * var param_1 = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index,val)
+ * obj.deleteAtIndex(index)
+ */
