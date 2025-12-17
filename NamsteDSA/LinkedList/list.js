52357deb18d55d456003afa39fd557bed1d31df7
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-12-17 12:06:57 +0530

    practiced linked list design

--- NamsteDSA/LinkedList/list.js
+++ NamsteDSA/LinkedList/list.js
@@ -1,22 +1,16 @@
-// linked list creation
+// LinkedList design
 
-function Node(val){
+
+var MyLinkedList = function(val) {
     this.val = val;
-    this.next = null;
-}
-
-
-// linked list initialization
-
-function myList(){
-    this.head = null;
-    this.size = 0
-}
-
-MyLinkedList.prototype.get = function(index) {
-    
+    this.next = null
 };
 
+var MyLinkedList = function(){
+    this.head = null;
+    this.size = 0
+
+}
 
 MyLinkedList.prototype.addAtHead = function(val) {
     let newNode = new Node(val)
@@ -36,4 +30,26 @@ MyLinkedList.prototype.addAtTail = function(val) {
         }
         curr.next = newNode
     }
+};
+
+
+MyLinkedList.prototype.addAtIndex = function(index, val) {
+    let newNode = new Node(val);
+    if(index === 0 ){
+        this.addAtHead(val);
+        return
+    }
+    else if (index === this.size){
+          this.addAtTail(val)
+          return
+    }
+    else{
+        let curr = this.head;
+        for(let i =0; i<index-1;i++){
+            curr= curr.next;
+        }
+        newNode.next = curr.next;
+        curr.next = newNode
+    }
+    this.size++
 };
