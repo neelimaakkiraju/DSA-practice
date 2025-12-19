5d0bc21e5dcf2d64c0a1a83fad35c00f940baaed
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-12-19 12:03:25 +0530

    practiced floyd's algorithm within linked list

--- NamsteDSA/LinkedList/hashTable.js
+++ NamsteDSA/LinkedList/hashTable.js
@@ -16,3 +16,21 @@ var hasCycle = function(head) {
     }
     return false
 };
+
+
+// floyd's Alogorithm
+
+var hasCycle = function(head) {
+    let slow = head;
+    let fast = head.next;
+
+    while(slow !== fast){
+        if(fast === null || fast.next === null){
+            return false
+        }
+        slow = slow.next;
+        fast = fast.nent.next;
+    }
+
+    return true;
+};
