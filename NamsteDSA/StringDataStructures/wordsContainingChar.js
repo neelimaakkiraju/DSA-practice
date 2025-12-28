bd7953c24dd3248f8745468fc508dd61826fe916
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-12-28 19:05:44 +0530

    solved it without using built in methods

--- NamsteDSA/StringDataStructures/wordsContainingChar.js
+++ NamsteDSA/StringDataStructures/wordsContainingChar.js
@@ -18,3 +18,23 @@ function findWordChar(str, char){
 }
 
 console.log(findWordChar("luffy is still joyboy", "z"))
+
+
+function findWordChar(str, char){
+    str = str.split(" ")
+    let result = []
+   
+    for(let i = 0; i<str.length;i++){
+       for(let j = 0; j<str[i].length;j++){
+        if(str[i][j] === char){
+            result.push(i);
+            break;
+        }
+       }
+    }
+
+    return result;
+   
+}
+
+console.log(findWordChar("luffy is still joyboy", "o"))
