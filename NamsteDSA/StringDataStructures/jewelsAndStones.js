f38ed8765b2392214bdfa96d8883f4f5c064036b
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-12-30 11:39:56 +0530

    solved using loops without built in methods

--- NamsteDSA/StringDataStructures/jewelsAndStones.js
+++ NamsteDSA/StringDataStructures/jewelsAndStones.js
@@ -1,25 +1,43 @@
 // find jewels and stones in given string
 
+//using built-in method
 
-function findJewels(str,stones){
-    
-    let count = 0
+function findJewels(str, stones) {
+  let count = 0;
 
-    for(let i = 0; i< stones.length;i++){
-      for(let j= 0; j< str.length;j++){
-         if(stones[i].includes(str[j])){
-            
-             count++
-       }
-      }
+  for (let i = 0; i < stones.length; i++) {
+    if (str.includes(stones[i])) {
+      count++;
     }
+  }
 
-    return count;
+  return count;
 }
 
-console.log(findJewels("aA","aAAbbbb"))
+console.log(findJewels("aA", "aAAbbbb"));
 
-console.log(findJewels("z",  "ZZ"))
+console.log(findJewels("z", "ZZ"));
 
+console.log(findJewels("badc", "ccc"));
 
-console.log(findJewels("badc",  "ccc"))
+// using loops
+
+function findJewels2(str, stones) {
+  let count = 0;
+
+  for (let i = 0; i < stones.length; i++) {
+    for (let j = 0; j < str.length; j++) {
+      if (stones[i] === str[j]) {
+        count++;
+      }
+    }
+  }
+
+  return count;
+}
+
+console.log(findJewels2("aA", "aAAbbbb"));
+
+console.log(findJewels2("z", "ZZ"));
+
+console.log(findJewels2("badc", "ccc"));
