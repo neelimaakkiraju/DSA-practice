c26f488163929014bd2c38a8dc1408c58596cbe6
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2025-12-31 16:06:04 +0530

    solved using object keys and 2nd approach

--- NamsteDSA/StringDataStructures/countVowelsAndCons.js
+++ NamsteDSA/StringDataStructures/countVowelsAndCons.js
@@ -42,23 +42,18 @@ function findMaxCount2(str) {
   let map = {};
 
   for (let i = 0; i < str.length; i++) {
-    if (!map[str[i]]) {
-      map[str[i]] = 1;
-    } else {
-      map[str[i]]++;
-    }
+    map[str[i]] = !map[str[i]] ? 1 : ++map[str[i]]
   }
 
   let vowels = ["a", "e", "i", "o", "u"];
   let maxVowels = 0;
   let maxCons = 0;
-  for (let i = 0; i < str.length; i++) {
-    if (vowels.includes(str[i])) {
-      if (map[str[i]] > maxVowels) {
-        maxVowels = map[str[i]];
-      }
-    } else if (map[str[i]] > maxCons) {
-      maxCons = map[str[i]];
+  let mapKeys = Object.keys(map)
+  for (let i = 0; i < mapKeys.length; i++) {
+    if (vowels.includes(mapKeys[i])) {
+     maxVowels = Math.max(maxVowels, map[mapKeys[i]])
+    } else {
+      maxCons = Math.max(maxCons, map[mapKeys[i]]);
     }
   }
 
