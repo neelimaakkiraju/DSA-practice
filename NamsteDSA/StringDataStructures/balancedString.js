91986801c488c0f07549ab98b3900e5055d8ca55
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2026-01-01 18:51:52 +0530

    solved balanced string using loops

--- NamsteDSA/StringDataStructures/balancedString.js
+++ NamsteDSA/StringDataStructures/balancedString.js
@@ -1,3 +1,30 @@
 // count balanced substrings in given string
 
 
+function balancedString(str){
+    let R = 0;
+    let L = 0;
+     let count = 0;
+
+     for(let i = 0 ; i < str.length; i++){
+        if(str[i] === "R"){
+            R = 1;
+        
+        }else{
+            L++
+        }
+
+        if(L === R){
+            count++;
+            L = 0;
+            R = 0
+        }
+     }
+
+     return count;
+}
+
+
+console.log(balancedString("RLRRLLRLRL"))
+
+console.log(balancedString("RLRRRLLRLL"))
