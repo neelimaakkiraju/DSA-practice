2db0e5ea519ed4a569bbc974dfd4ac6bcbe0cf62
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2026-01-07 11:24:21 +0530

    solved approach2 in optimized method, without using built in

--- NamsteDSA/StringDataStructures/validPalindrome.js
+++ NamsteDSA/StringDataStructures/validPalindrome.js
@@ -21,3 +21,27 @@ function checkStr(str){
 console.log(checkStr("A man, a plan, a canal: Panama"))
 
 console.log(checkStr("race a car"))
+
+
+
+// approach 2
+function checkStr2(str){
+    str = str.toLowerCase();
+
+    let filteredStr = "";
+    let result = ""
+    for(let i =0; i< str.length; i++){
+        if(str[i].match(/[a-z0-9]/i)){
+            filteredStr += str[i];
+            result = str[i] += result
+        }
+    }
+
+
+    return result === filteredStr 
+}
+
+console.log(checkStr2("A man, a plan, a canal: Panama"))
+
+console.log(checkStr2("race a car"))
+
