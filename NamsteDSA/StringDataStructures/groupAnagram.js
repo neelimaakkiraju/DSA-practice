39337c3af469c0698d1157e8547dd7ed24aa8ae6
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2026-01-13 15:31:28 +0530

    solved group anagram using hash map and hash key

--- NamsteDSA/StringDataStructures/groupAnagram.js
+++ NamsteDSA/StringDataStructures/groupAnagram.js
@@ -2,8 +2,9 @@
 
 function groupAnagram(str){
     let map = {}
+    
     for(let i = 0; i<str.length;i++){
-
+        
         let sortedArr = str[i].split("").sort().join("")
         if(!map[sortedArr]){
             map[sortedArr] = [str[i]]
@@ -16,3 +17,36 @@ function groupAnagram(str){
 }
 
 console.log(groupAnagram(["eat","tea","tan","ate","nat","bat"]))
+
+
+// using hash key
+
+
+  var groupAnagrams = function(strs) {
+      let map = {};
+  
+      for (let i = 0; i < strs.length; i++) {
+          let freqArr = Array(26).fill(0);
+          let s = strs[i];
+  
+          for (let j = 0; j < s.length; j++) {
+              let index = s[j].charCodeAt(0) - 'a'.charCodeAt(0);
+              freqArr[index]++;
+          }
+  
+          let key = "";
+          for (let k = 0; k < 26; k++) {
+              key += "#" + freqArr[k];
+          }
+  
+          if (!map[key]) {
+              map[key] = [s];
+          } else {
+              map[key].push(s);
+          }
+      }
+  
+      return Object.values(map);
+  };
+
+  console.log(groupAnagram(["eat","tea","tan","ate","nat","bat"]))
