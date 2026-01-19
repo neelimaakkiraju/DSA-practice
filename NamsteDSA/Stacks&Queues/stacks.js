bda7d058feb3c7da0aec2347a69b252ef3ec6f18
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2026-01-19 11:37:20 +0530

    implemented stacks using single queue

--- NamsteDSA/Stacks&Queues/stacks.js
+++ NamsteDSA/Stacks&Queues/stacks.js
@@ -36,3 +36,35 @@ MyStack.prototype.top = function() {
 MyStack.prototype.empty = function() {
   return this.q1.length === 0;
 };
+
+// implement stacks using one queue
+
+var MyStack = function() {
+    this.q = []; 
+};
+
+MyStack.prototype.push = function(x) {
+    this.q.push(x);
+};
+
+MyStack.prototype.pop = function() {
+    let n = this.q.length;
+    for (let i = 0; i < n - 1; i++) {
+        this.q.push(this.q.shift());
+    }
+    return this.q.shift();
+};
+
+MyStack.prototype.top = function() {
+    let n = this.q.length;
+    for (let i = 0; i < n - 1; i++) {
+        this.q.push(this.q.shift());
+    }
+    let front = this.q.shift();
+    this.q.push(front);
+    return front;
+};
+
+MyStack.prototype.empty = function() {
+    return this.q.length === 0;
+};
