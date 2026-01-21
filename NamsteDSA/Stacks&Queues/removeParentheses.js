9561d872a0dd1655d3aeb4b0e878ddb9f010b3ff
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2026-01-21 15:59:44 +0530

    optimized code

--- NamsteDSA/Stacks&Queues/removeParentheses.js
+++ NamsteDSA/Stacks&Queues/removeParentheses.js
@@ -1,29 +1,27 @@
 // Remove outer most parentheses
 
-function removeParentheses(s){
-    let stack = [];
-    let ans = ""
+function removeParentheses(s) {
+  let stack = [];
+  let ans = "";
 
-    for(let i = 0; i<s.length; i++){
-        if(s[i] === "("){
-            stack.push(s[i])
-            
-        let len = stack.length;
-        if(len > 1){
-            ans = ans+ s[i]
-        }
-        }else{
-            
-        let len = stack.length;
-        if(len > 1){
-            ans = ans+ s[i]
-        }
-            stack.pop()
-        }
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+      stack.push(s[i]);
 
+      
+        ans += ((stack.length > 1)? s[i] : "")
+      
+    } else {
+      if (stack.length > 1) {
+        ans = ans + s[i];
+      }
+      stack.pop();
     }
+  }
 
-    return ans;
+  return ans;
 }
 
-console.log(removeParentheses("(()())(())"))
+console.log(removeParentheses("(()())(())"));
+
+console.log(removeParentheses("(()())(())(()(()))"));
