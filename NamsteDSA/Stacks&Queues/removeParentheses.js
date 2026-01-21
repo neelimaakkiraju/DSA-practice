76d97751cea8e9b9516636ee222c640a579a531f
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2026-01-21 16:12:41 +0530

    solved remove parentheses without using stack

--- NamsteDSA/Stacks&Queues/removeParentheses.js
+++ NamsteDSA/Stacks&Queues/removeParentheses.js
@@ -25,3 +25,28 @@ function removeParentheses(s) {
 console.log(removeParentheses("(()())(())"));
 
 console.log(removeParentheses("(()())(())(()(()))"));
+
+// Remove outer most parentheses without using stack
+
+function removeParentheses2(s) {
+  let level = 0;
+  let ans = "";
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "(") {
+        ++level
+      
+        ans += ((level > 1)? s[i] : "")
+      
+    } else {
+       ans += ((level > 1)? s[i] : "")
+     --level
+    }
+  }
+
+  return ans;
+}
+
+console.log(removeParentheses2("(()())(())"));
+
+console.log(removeParentheses2("(()())(())(()(()))"));
