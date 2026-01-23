fec59d736ce0006add398d8cb1e6a1b92bc343e0
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2026-01-23 18:29:00 +0530

    solved greater temperature using stacks

---
+++ NamsteDSA/Stacks&Queues/greaterTemp.js
@@ -0,0 +1,23 @@
+// find next greater temperature in an array using stacks
+
+
+var dailyTemperatures = function(arr) {
+    let stack = [];
+    let n = arr.length;
+    let ans = Array(n).fill(0);
+
+    stack.push(n-1);
+    for(let i = n-2; i >= 0; i--){
+        while(stack.length) {
+            let top = stack[stack.length-1];
+            if(arr[i] >= arr[top]) {
+                stack.pop();
+            } else {
+                ans[i] = top - i;
+                break;
+            }
+        }
+        stack.push(i);
+    }
+    return ans;
+};
