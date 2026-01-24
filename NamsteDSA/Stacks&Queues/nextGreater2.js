81e33899f045ad625f1333b474e94a1d5b1bb8d3
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2026-01-24 16:07:45 +0530

    solved next greater using stack in circular iteration

---
+++ NamsteDSA/Stacks&Queues/nextGreater2.js
@@ -0,0 +1,24 @@
+// find next greater in circular array
+
+var nextGreaterElements = function(arr) {
+    let n = arr.length;
+    let stack = [];
+    let ans = Array(n).fill(-1);
+    stack.push(arr[n-1]);
+    for(let i=(2*n)-2; i >= 0; i--){
+        while(stack.length){
+            let top = stack[stack.length-1];
+            if(arr[i % n] < top){
+                ans[i % n] = top;
+                break;
+            } else{
+                stack.pop();
+            }
+        }
+        stack.push(arr[i % n]);
+    }
+    return ans.slice(0, n);
+};
+
+
+console.log(nextGreaterElements([1,2,3,4,3]))
