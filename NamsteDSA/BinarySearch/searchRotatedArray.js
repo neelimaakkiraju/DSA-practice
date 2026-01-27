28ab06eabf974fa6eda2a675e4d2ec272b65e0e7
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2026-01-27 15:47:43 +0530

    solved search number in rotated sorted array using binary search

---
+++ NamsteDSA/BinarySearch/searchRotatedArray.js
@@ -0,0 +1,31 @@
+// search target element in rotated sorted array
+
+// using binary search
+var search = function(arr, target) {
+    let l = 0;
+    let r = arr.length - 1;
+    while (l <= r) {
+        let m = l + Math.floor((r - l) / 2);
+        if (target === arr[m]) {
+            return m;
+        }
+        
+        if (arr[l] <= arr[m]) {
+            if (target >= arr[l] && target < arr[m]) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        } else {
+            if (target > arr[m] && target <= arr[r]) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+    }
+    return -1;
+};
+
+console.log(search([1,5,8,9,4,3], 8))
+
