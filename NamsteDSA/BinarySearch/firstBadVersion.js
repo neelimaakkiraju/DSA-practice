3303d7ab8a80d273aaea95a2cc52a64d038b565a
Neelima <105192660+neelimaakkiraju@users.noreply.github.com>
2026-01-28 11:47:55 +0530

    solved first bad version using binary search

---
+++ NamsteDSA/BinarySearch/firstBadVersion.js
@@ -0,0 +1,27 @@
+// find first bad version using binary search
+
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let l = 1;
+        let r = n;
+
+        while(l < r){
+            let m = l + Math.floor((r - l)/2);
+            if(!isBadVersion(m)){
+                l = m + 1
+            }else{
+                r = m
+            }
+        }
+        return r
+    };
+};
